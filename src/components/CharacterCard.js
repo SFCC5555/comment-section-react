28e8166e29ba731c30bd7fbdb4bc3a252ccc6a8b
sfcc5555 <sfcc.5555@gmail.com>
2023-03-21 20:24:13 -0500

    ac

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,7 +2,7 @@ import '../styles/CharacterCard.css'
 
 function CharacterCard(props) {
     return (<section className='characterCard'>
-                <img src={require(`\../Images/fotoPerfil${props.name}.png`)} alt={`foto de ${props.name}`} />
+                <img src={require(`../Images/fotoPerfil${props.name}.png`)} alt={`foto de ${props.name}`} />
                 <section className="dataSection">
                     <div className="name"><strong>{props.name} {props.lastname}</strong> en {props.country}</div>
                     <div className="position">{props.position} en <strong>{props.company}</strong></div>
